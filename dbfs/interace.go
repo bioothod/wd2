d92adc16dfa651fbe497212967c7170ae3470ce2
Evgeniy Polyakov <zbr@ioremap.net>
2016-10-03 22:41:04 +0300

    interface: remove data from elliptics only if there is bucket, added alot of debug

--- dbfs/interace.go
+++ dbfs/interace.go
@@ -47,7 +47,25 @@ func (ctl *DbFSUser) OpenFile(name string, flags int, perm os.FileMode) (webdav.
 	ent := NewDirEntry(ctl.Username, name)
 	ent.Fmode = perm
 
-	glog.Infof("openfile: username: %s, filename: %s, flags: %x, perm: %s", ctl.Username, name, flags, perm.String())
+	flags_array := make([]string, 0)
+	if (flags & os.O_CREATE) != 0 {
+		flags_array = append(flags_array, "create")
+	}
+	if (flags & os.O_TRUNC) != 0 {
+		flags_array = append(flags_array, "truncate")
+	}
+	if (flags & os.O_RDWR) != 0 {
+		flags_array = append(flags_array, "rdwr")
+	}
+	if (flags & os.O_WRONLY) != 0 {
+		flags_array = append(flags_array, "wronly")
+	} else {
+		flags_array = append(flags_array, "rdonly")
+	}
+	if (flags & os.O_APPEND) != 0 {
+		flags_array = append(flags_array, "append")
+	}
+	glog.Infof("openfile: username: %s, filename: %s, flags: %x %v, perm: %s", ctl.Username, name, flags, flags_array, perm.String())
 
 	if ent.Filename == "/" {
 		if flags & (os.O_WRONLY | os.O_RDWR) != 0 {
@@ -62,6 +80,8 @@ func (ctl *DbFSUser) OpenFile(name string, flags int, perm os.FileMode) (webdav.
 			if err != nil {
 				return nil, fmt.Errorf("openfile: could not create empty file: %v", err)
 			}
+			glog.Infof("openfile: username: %s, filename: %s, flags: %x %v, perm: %s: created new file",
+				ctl.Username, name, flags, flags_array, perm.String())
 		} else {
 			glog.Errorf("openfile: could not stat file: %v", err)
 			return nil, os.ErrNotExist
@@ -75,6 +95,9 @@ func (ctl *DbFSUser) OpenFile(name string, flags int, perm os.FileMode) (webdav.
 		if err != nil {
 			return nil, fmt.Errorf("openfile: truncate failed: %v", err)
 		}
+
+		glog.Infof("openfile: username: %s, filename: %s, flags: %x %v, perm: %s: updated entry: %s",
+			ctl.Username, name, flags, flags_array, perm.String(), ent.String())
 	}
 
 	f := &File {
@@ -94,24 +117,38 @@ func (ctl *DbFSUser) RemoveAll(name string) error {
 
 	err := ctl.FS.StatEntry(ent)
 	if err != nil {
+		glog.Errorf("remove: username: %s, filename: %s: there is no directory entry: %s", ctl.Username, name, err)
 		return err
 	}
 
 	err = ctl.FS.DeleteEntry(ent)
 	if err != nil {
+		glog.Errorf("remove: %s: could not delete entry: %s", ent.String(), err)
 		return err
 	}
+
+	glog.Infof("remove: %s: entry deleted", ent.String())
 
 	if ent.IsDir() {
 		return nil
 	}
 
-	f := &File {
-		User: ctl,
-		Info: ent,
+	if ent.Bucket != "" {
+		f := &File {
+			User: ctl,
+			Info: ent,
+		}
+
+		err = f.RemoveData()
+		if err != nil {
+			glog.Errorf("remove: %s: could not remove data from elliptics: %v", ent.String(), err)
+			return err
+		}
+
+		glog.Infof("remove: %s: entry deleted from elliptics", ent.String())
 	}
 
-	return f.RemoveData()
+	return nil
 }
 
 func (ctl *DbFSUser) Rename(oldName, newName string) error {
@@ -137,6 +174,8 @@ func (ctl *DbFSUser) Rename(oldName, newName string) error {
 	// check whether src object exists
 	err := ctl.FS.StatEntry(oent)
 	if err != nil {
+		glog.Errorf("rename: username: %s, filename: %s -> %s: there is no old directory entry: %s",
+			ctl.Username, oldName, newName, err)
 		return err
 	}
 
@@ -145,7 +184,8 @@ func (ctl *DbFSUser) Rename(oldName, newName string) error {
 		err := ctl.FS.StatEntry(nent)
 		if err == nil {
 			if !nent.IsDir() {
-				return fmt.Errorf("move: %s -> %s: destination is not a directory", oent.Filename, nent.Filename)
+				glog.Errorf("rename: %s -> %s: destination is not a directory", oent.String(), nent.String())
+				return fmt.Errorf("rename: %s -> %s: destination is not a directory", oent.Filename, nent.Filename)
 			}
 
 			nf := &File {
@@ -155,11 +195,15 @@ func (ctl *DbFSUser) Rename(oldName, newName string) error {
 
 			fi, err := nf.Readdir(0)
 			if err != nil {
+				glog.Errorf("rename: %s -> %s: src readdir failed: %v", oent.String(), nent.String(), err)
 				return err
 			}
 
 			if len(fi) != 0 {
-				return fmt.Errorf("move: %s -> %s: destination directory is not empty (%d entries)",
+				glog.Errorf("rename: %s -> %s: destination directory is not empty (%d entries)",
+					oent.String(), nent.String(), len(fi))
+
+				return fmt.Errorf("rename: %s -> %s: destination directory is not empty (%d entries)",
 					oent.Filename, nent.Filename, len(fi))
 			}
 		}
@@ -167,6 +211,7 @@ func (ctl *DbFSUser) Rename(oldName, newName string) error {
 
 	err = ctl.FS.DeleteEntry(oent)
 	if err != nil {
+		glog.Errorf("rename: %s -> %s: could not delete old entry: %v", oent.String(), nent.String(), err)
 		return err
 	}
 
@@ -179,6 +224,7 @@ func (ctl *DbFSUser) Rename(oldName, newName string) error {
 
 	err = ctl.FS.InsertEntry(nent)
 	if err != nil {
+		glog.Errorf("rename: %s -> %s: could not insert new entry: %v", oent.String(), nent.String(), err)
 		return err
 	}
 
@@ -193,6 +239,7 @@ func (ctl *DbFSUser) Rename(oldName, newName string) error {
 
 	fi, err := of.Readdir(0)
 	if err != nil {
+		glog.Errorf("rename: %s -> %s: dst readdir failed: %v", oent.String(), nent.String(), err)
 		return err
 	}
 
@@ -202,6 +249,8 @@ func (ctl *DbFSUser) Rename(oldName, newName string) error {
 
 		err = ctl.Rename(src, dst)
 		if err != nil {
+			glog.Errorf("rename: %s -> %s: recursive rename failed: %s -> %s, error: %v",
+				oent.String(), nent.String(), src, dst, err)
 			return err
 		}
 	}
